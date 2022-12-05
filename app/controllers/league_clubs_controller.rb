05640e06dfea309aa5b439990f72cb4d37f1f2fb
Alastair Lloyd <dex8056@aol.com>
2022-12-05 16:35:50 -0700

    complete user story 13

--- app/controllers/league_clubs_controller.rb
+++ app/controllers/league_clubs_controller.rb
@@ -3,4 +3,14 @@ class LeagueClubsController < ApplicationController
     @league = League.find(params[:league_id])
     @clubs = Club.where(league_id: params[:league_id])
   end
+
+  def new
+    @league = League.find(params[:id])
+  end
+
+  def create 
+    league = League.find(params[:id])
+    club = league.clubs.create!(name: params[:name], position: params[:position], city: params[:city], previous_winner: params[:previous_winner])
+    redirect_to "/leagues/#{league.id}/clubs"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   get '/clubs', to: 'clubs#index'
   get '/clubs/:id', to: 'clubs#show'
   get '/leagues/:league_id/clubs', to: 'league_clubs#index'
+  get '/leagues/:id/clubs/new', to: 'league_clubs#new'
   
   post '/leagues', to: 'leagues#create'
+  post '/leagues/:id/clubs', to: 'league_clubs#create'
 
   patch '/leagues/:id', to: 'leagues#update'
 end
